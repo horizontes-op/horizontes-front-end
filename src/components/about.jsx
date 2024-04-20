f36019896cd6a3979c38c826e9a159e3842160b8
alanmath <alanmatheus187@gmail.com>
2024-04-20 19:23:06 -0300

    piloto

---
+++ src/components/about.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+// import { video } from "img/videoAp.mp4";
+
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+              <video src="/img/videoAp.mp4" className="img-responsive" alt="Descrição do vídeo" controls preload="auto"></video>{" "}
+            </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>About Us</h2>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <h3>Why Choose Us?</h3>
+              <div className="list-style">
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> {d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
